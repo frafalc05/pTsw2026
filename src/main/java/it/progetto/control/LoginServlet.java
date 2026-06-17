cde3300e1a1d99317993edb7e71f71fb64da1e6c
frafalc05 <francesca05falcone@gmail.com>
2026-06-17 15:53:25 +0200

    Logout Login Servlet

---
+++ src/main/java/it/progetto/control/LoginServlet.java
@@ -0,0 +1,45 @@
+package it.progetto.control;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import it.progetto.dao.UtenteDAO;
+import it.progetto.model.Utente;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/view/user/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UtenteDAO dao = new UtenteDAO();
+
+        try {
+            Utente utente = dao.login(email, password);
+
+            if (utente != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("utente", utente);
+                session.setAttribute("ruolo", utente.getRuolo());
+                response.sendRedirect(request.getContextPath() + "/home");
+            } else {
+                request.setAttribute("errore", "Email o password non corretti");
+                request.getRequestDispatcher("/WEB-INF/view/user/login.jsp").forward(request, response);
+            }
+
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
+    }
+}
---
+++ src/main/java/it/progetto/control/LogoutServlet.java
@@ -0,0 +1,18 @@
+package it.progetto.control;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getSession().invalidate();
+        response.sendRedirect(request.getContextPath() + "/home");
+    }
+}
