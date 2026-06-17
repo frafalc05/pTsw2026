9ee43874d827d392698a2b6e9b30ef7c03154e25
frafalc05 <francesca05falcone@gmail.com>
2026-06-17 15:36:30 +0200

    Test Connessione al database

---
+++ src/main/java/it/progetto/dao/DataSourceProvider.java
@@ -0,0 +1,44 @@
+package it.progetto.dao;
+import javax.sql.DataSource;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+
+public class DataSourceProvider {
+
+
+	private static DataSource dataSource;
+
+
+	public static DataSource getDataSource(){
+
+
+		if(dataSource == null){
+
+
+			MysqlDataSource ds = new MysqlDataSource();
+
+
+			ds.setURL(
+			"jdbc:mysql://localhost:3306/fiorista_maria"
+			);
+
+
+			ds.setUser("root");
+
+
+			ds.setPassword("root");
+
+
+			dataSource = ds;
+
+
+		}
+
+
+		return dataSource;
+
+
+	}
+
+}
