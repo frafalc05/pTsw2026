99484772ce1c6b0ff86be0de4d4a68dd4087e25a
frafalc05 <francesca05falcone@gmail.com>
2026-06-18 08:16:09 +0200

    Modifica UtenteDao

--- src/main/java/it/progetto/dao/UtenteDAO.java
+++ src/main/java/it/progetto/dao/UtenteDAO.java
@@ -8,25 +8,21 @@ import it.progetto.model.Utente;
 
 public class UtenteDAO {
 
-    public void insertUtente(Utente u) throws SQLException {
-        String sql = "INSERT INTO utente (nome, cognome, email, password, ruolo) VALUES (?, ?, ?, ?, ?)";
+	public void insertUtente(Utente u) throws SQLException {
+	    String sql = "INSERT INTO utente (nome, cognome, email, password, ruolo) VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection con = DataSourceProvider.getDataSource().getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
+	    try (Connection con = DataSourceProvider.getDataSource().getConnection();
+	         PreparedStatement ps = con.prepareStatement(sql)) {
 
-            ps.setString(1, u.getNome());
-            ps.setString(2, u.getCognome());
-            ps.setString(3, u.getEmail());
+	        ps.setString(1, u.getNome());
+	        ps.setString(2, u.getCognome());
+	        ps.setString(3, u.getEmail());
+	        ps.setString(4, u.getPassword()); 
+	        ps.setString(5, u.getRuolo());   
 
-            ps.setString(4, u.getPassword()); // già hashata nella servlet
-            ps.setString(5, "USER");
-            ps.setString(4, u.getPassword());
-            ps.setString(5, u.getRuolo());
-          
-
-            ps.executeUpdate();
-        }
-    }
+	        ps.executeUpdate();
+	    }
+	}
 
     public Utente login(String email, String password) throws SQLException {
         String passwordHash = DigestUtils.sha256Hex(password);
