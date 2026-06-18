82c2734075f968440a6aa47ecdb1159e5935f131
frafalc05 <francesca05falcone@gmail.com>
2026-06-18 09:46:18 +0200

    modifica di home servlet in modo tale che si apra come pagina iniziale home

--- src/main/java/it/progetto/control/HomeServlet.java
+++ src/main/java/it/progetto/control/HomeServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = {"", "/home"})
 public class HomeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
