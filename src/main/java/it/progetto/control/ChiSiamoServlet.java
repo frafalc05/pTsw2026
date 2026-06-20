5c5eee3f65f57f26520a1d10155c97e1489d6a68
d4nitra <d.ruocco10@studenti.unisa.it>
2026-06-20 16:22:14 +0200

    Aggiunta ChiSiamoServlet

---
+++ src/main/java/it/progetto/control/ChiSiamoServlet.java
@@ -0,0 +1,18 @@
+package it.progetto.control;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/chi-siamo")
+public class ChiSiamoServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/view/user/chi-siamo.jsp").forward(request, response);
+    }
+}
